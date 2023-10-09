52aa9a879eb742fbd776553eb3f69da88aac18f8
kunal yadav <kunalyadav1601@gmail.com>
2023-10-10 00:49:59 +0530

    all files added

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import Navbar from "../features/navbar/Navbar";
+import ProductList from "../features/product-list/ProductList";
+
+
+
+const Home = ()=>{
+    return (
+    <>
+    <Navbar>
+      
+    </Navbar>
+    <ProductList></ProductList>
+    </>
+    )
+};
+export default Home;
